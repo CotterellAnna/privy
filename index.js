544b4bb5b25015eb3e7986a19fbdd05c65dad8d0
CotterellAnna <cotterellanna@gmail.com>
2021-09-10 09:01:11 +0100

    fixed permission policy

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ function saveNames(e){
 
     let current_href = window.location.href;
     let current_pathname = window.location.pathname;
-    window.location.href = window.location.href.replace(current_href, `${current_pathname}chatRoom.html`);
+
+    if(current_pathname[current_pathname.length - 1] == '/')
+    current_pathname = current_pathname.replace(current_pathname.length - 1, '');
+
+    window.location.href = window.location.href.replace(current_href, `${current_pathname}/chatRoom.html`);
 }
 
 let register = document.getElementsByClassName('register');
